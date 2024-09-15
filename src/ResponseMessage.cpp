5b79517f882ff5c37920530512bf838ae208e624
josip <josipkeresman@gmail.com>
2024-09-15 16:12:26 +0200

    Fix formatting issues in ResponseMsg

--- src/ResponseMessage.cpp
+++ src/ResponseMessage.cpp
@@ -5,6 +5,8 @@ namespace justanlsp
 
 ResponseMessage::ResponseMessage(const std::string &jsonRPC, const std::string &result,
                                  const ResponseError &responseError)
-    : Message(jsonRPC), m_result(result), m_responseError(responseError) {}
+    : Message(jsonRPC), m_result(result), m_responseError(responseError)
+{
+}
 
 } // namespace justanlsp
