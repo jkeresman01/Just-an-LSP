1026ef74cdc21b5ccae6ab29bfc25415e9bf1c04
josip <josipkeresman@gmail.com>
2024-10-19 23:09:10 +0200

    Document the Message

--- src/headers/Message.h
+++ src/headers/Message.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <nlohmann/json.hpp>
 
 #include <string>
@@ -7,14 +8,41 @@
 namespace justanlsp
 {
 
+//////////////////////////////////////////////////////////////
+///
+/// @class Message
+///
+/// @brief A general message as defined by JSON-RPC.
+///
+//////////////////////////////////////////////////////////////
 class Message
 {
   public:
-    Message(const std::string &jsonRPC) : m_jsonRPC(jsonRPC){};
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Constructs the Message from jsonRPC request
+    ///
+    /// @param jsonRPC
+    ///
+    //////////////////////////////////////////////////////////////
+    Message(const nlohmann::json &jsonRPC) : m_jsonRPC(jsonRPC["jsonrpc"]){};
 
-    virtual nlohmann::json toJson() const;
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief  Serializes the message to JSON format
+    ///
+    /// @return Returns serialized Message in JSON format
+    ///
+    //////////////////////////////////////////////////////////////
+    virtual nlohmann::json toJson() const = 0;
 
   protected:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief RPC version, language server protocol always uses "2.0"
+    ///        as the jsonrpc version
+    ///
+    //////////////////////////////////////////////////////////////
     std::string m_jsonRPC;
 };
 
