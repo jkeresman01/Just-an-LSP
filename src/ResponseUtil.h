cdd3a2d89d7a95b5a1007f473440421b79617d5d
josip <josipkeresman@gmail.com>
2024-10-20 12:15:12 +0200

    Implement Response Utility class

---
+++ src/ResponseUtil.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "ResponseMessage.h"
+
+namespace justanlsp
+{
+class ResponseUtil
+{
+  public:
+    static void sendResponse(const nlohmann::json &response)
+    {
+        LOG_INFO("Sending response: ");
+        LOG_INFO(response.dump(4));
+
+        std::cout << "Content-Length: " << response.dump().size() << "\r\n\r\n";
+        std::cout << response.dump() << std::endl;
+    }
+};
+} // namespace justanlsp
