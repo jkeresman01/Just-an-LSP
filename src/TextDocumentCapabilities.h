9bf3163385a3786566cb8bd21d3ca30324a02912
josip <josipkeresman@gmail.com>
2024-10-20 18:57:23 +0200

    Implement TextDocumentSyncCapabilities

--- src/TextDocumentCapabilities.h
+++ src/TextDocumentCapabilities.h
@@ -1,10 +1,46 @@
 #pragma once
 
+#include <memory>
+#include <nlohmann/json.hpp>
+
 namespace justanlsp
 {
+
+//////////////////////////////////////////////////////////////
+///
+/// @class TextDocumentCapabilities
+///
+/// @brief TextDocumentClientCapabilities define capabilities
+///        the editor / tool provides on text documents.
+///
+//////////////////////////////////////////////////////////////
 class TextDocumentCapabilities
 {
-  public:
+    // TODO reset of the capabilities only sync and hover are currently included
 
+  public:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Constructs the TextDocumentCapabilities from jsonRPC request
+    ///
+    /// @param jsonRPC
+    ///
+    //////////////////////////////////////////////////////////////
+    TextDocumentCapabilities(const nlohmann::json &jsonRPC);
+
+  private:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief  Capabilities specific to the textDocument syncrhonization
+    ///
+    //////////////////////////////////////////////////////////////
+    TextDocumentSyncClientCapabilities m_synchronization;
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief  Capabilities specific to the `textDocument/hover` request.
+    ///
+    //////////////////////////////////////////////////////////////
+    HoverClientCapabilities m_hover;
 };
 } // namespace justanlsp
