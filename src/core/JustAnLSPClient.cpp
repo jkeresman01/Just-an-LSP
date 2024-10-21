f08db1d9a25709414e10db8a3ac2f798bc02d334
josip <josipkeresman@gmail.com>
2024-10-21 18:31:59 +0200

    Implement basic JustAnLSPClient

---
+++ src/core/JustAnLSPClient.cpp
@@ -0,0 +1,16 @@
+#include "JustAnLSPClient.h"
+
+#include <memory>
+
+namespace justanlsp
+{
+    void JustAnLSPClient::saveInfo(const ClientInfo &clientInfo)
+    {
+        m_clientInfo = clientInfo;
+    }
+
+    void JustAnLSPClient::registerCapabilites(const std::shared_ptr<ClientCapabilities> &clientCapabilites)
+    {
+        m_clientCapabilities = clientCapabilites;
+    }
+}
