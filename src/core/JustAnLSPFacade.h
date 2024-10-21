17a5ea9c99438e5d3847994c9805e141578d8c6b
josip <josipkeresman@gmail.com>
2024-10-21 21:04:38 +0200

    Document the JustAnLSPFacade

--- src/core/JustAnLSPFacade.h
+++ src/core/JustAnLSPFacade.h
@@ -9,21 +9,98 @@
 namespace justanlsp
 {
 
+//////////////////////////////////////////////////////////////
+///
+/// @class JustAnLSPFacade
+///
+/// @brief Single simplified interface for handling JustAnLspLSP 
+///        request messages
+///
+//////////////////////////////////////////////////////////////
 class JustAnLSPFacade
 {
   public:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Default constructor
+    ///
+    //////////////////////////////////////////////////////////////
     JustAnLSPFacade() = default;
 
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Interface for handling all LSP requests
+    ///
+    /// @param String representation of incoming request
+    ///
+    //////////////////////////////////////////////////////////////
     ResponseMessage handleRequest(const std::string &request);
 
   private:
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Entry point for handling initialize request
+    ///
+    /// @param string representation of incoming request
+    ///
+    /// @return responseMessage
+    ///
+    //////////////////////////////////////////////////////////////
     ResponseMessage handleInitializeRequest(const std::string &request);
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Entry point for handling initialized request
+    ///
+    /// @param string representation of incoming request
+    ///
+    /// @return responseMessage
+    ///
+    //////////////////////////////////////////////////////////////
     ResponseMessage handleInitializedRequest(const std::string &request);
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Entry point for handling textDocument/didOpen request
+    ///
+    /// @param string representation of incoming request
+    ///
+    /// @return responseMessage
+    ///
+    //////////////////////////////////////////////////////////////
     ResponseMessage handleTextDocumentDidOpenRequest(const std::string &request);
-    ResponseMessage handleTextDocumentHoverRequest(const std::string &request);
+
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Entry point for handling textDocument/didChange request
+    ///
+    /// @param string representation of incoming request
+    ///
+    /// @return responseMessage
+    ///
+    //////////////////////////////////////////////////////////////
     ResponseMessage handleTextDocumentDidChangeRequest(const std::string &request);
 
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Entry point for handling textDocument/hover request
+    ///
+    /// @param string representation of incoming request
+    ///
+    /// @return responseMessage
+    ///
+    //////////////////////////////////////////////////////////////
+    ResponseMessage handleTextDocumentHoverRequest(const std::string &request);
+
   private:
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Registered client (editor or tool)
+    ///
+    //////////////////////////////////////////////////////////////
     std::shared_ptr<JustAnLSPClient> m_client = std::make_shared<JustAnLSPClient>();
 };
 
