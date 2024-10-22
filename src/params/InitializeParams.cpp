efa970668d0cd71319aa6303f42c524f88338c39
josip <josipkeresman@gmail.com>
2024-10-22 14:44:48 +0200

    Fix some random stuff in InitializeParams impl

--- src/params/InitializeParams.cpp
+++ src/params/InitializeParams.cpp
@@ -6,15 +6,15 @@
 
 namespace justanlsp
 {
+
 InitializeParams::InitializeParams(const nlohmann::json &jsonRPC)
     : m_processId(jsonRPC["proccessId"]), m_locale(jsonRPC["locale"])
 {
     nlohmann::json clientInfoJson = jsonRPC["clientInfo"];
-
     m_clientInfo.name = clientInfoJson["name"];
     m_clientInfo.version = clientInfoJson["version"];
 
-    auto it = jsonRPC.find("trace");
+    nlohmann::json it = jsonRPC.find("trace");
     if (it == jsonRPC.end())
     {
         m_traceValue = TraceValue::OFF;
@@ -25,4 +25,5 @@ InitializeParams::InitializeParams(const nlohmann::json &jsonRPC)
         m_traceValue = TraceUtil::getTraceValue(traceStr);
     }
 }
+
 } // namespace justanlsp
