c1f13492a3ef9eb1665e610f5a36f5e06ea61f37
josip <josipkeresman@gmail.com>
2024-10-25 22:50:41 +0200

    Add toString() to ResponseError

--- src/messages/ResponseError.cpp
+++ src/messages/ResponseError.cpp
@@ -1,10 +1,22 @@
 #include "ResponseError.h"
 
+#include <sstream>
+
 namespace justanlsp
 {
+
 ResponseError::ResponseError(const ErrorCodes &code, const std::string &message)
     : m_code(code), m_message(message)
 {
+
+}
+
+std::string ResponseError::toString() const
+{
+    std::stringstream ss;
+    ss << "Error code { " << int(m_code) << " }, ";
+    ss << "Error msg { " << m_message << " }, ";
+    return ss.str();
 }
 
 } // namespace justanlsp
