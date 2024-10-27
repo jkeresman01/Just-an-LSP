085e6fcc241f32115c0b8f19c9f4b9ddb856db2c
josip <josipkeresman@gmail.com>
2024-10-27 19:42:53 +0100

    Implement DidOpenTextDocumentParams

---
+++ src/params/DidOpenTextDocumentParams.cpp
@@ -0,0 +1,27 @@
+#include "DidOpenTextDocumentParams.h"
+
+#include <memory>
+
+#include "../utils/Logger.h"
+
+namespace justanlsp
+{
+DidOpenTextDocumentParams::DidOpenTextDocumentParams(const nlohmann::json &jsonRPC)
+{
+    setTextDocumentItemKind(jsonRPC);
+}
+
+void DidOpenTextDocumentParams::setTextDocumentItemKind(const nlohmann::json &jsonRPC)
+{
+    auto it = jsonRPC.find("textDocument");
+
+    if (it == jsonRPC.end())
+    {
+        LOG_ERROR << "No textDocument in textDocument/didOpen request";
+    }
+    else
+    {
+        m_textDocumentItem = std::make_shared<TextDocumentItem>(jsonRPC["textDocument"]);
+    }
+}
+} // namespace justanlsp
--- src/params/DidOpenTextDocumentParams.h
+++ src/params/DidOpenTextDocumentParams.h
@@ -1,17 +1,61 @@
 #pragma once
 
+#include <memory>
+#include <nlohmann/json.hpp>
+
 #include "../types/TextDocumentItem.h"
 
 namespace justanlsp
 {
+
+//////////////////////////////////////////////////////////////
+///
+/// @class DidOpenTextDocumentParams
+///
+/// @brief Represents the parameters sent when a text document
+///        is opened in the Language Server Protocol (LSP).
+///
+//////////////////////////////////////////////////////////////
 class DidOpenTextDocumentParams
 {
   public:
-      DidOpenTextDocumentParams(const nlohamnn::json &jsonRPC);
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Constructs a DidOpenTextDocumentParams object
+    ///        from a JSON-RPC request.
+    ///
+    /// @param jsonRPC
+    ///
+    //////////////////////////////////////////////////////////////
+    DidOpenTextDocumentParams(const nlohmann::json &jsonRPC);
 
-      nlohmann::json toJson() const;
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Accessor method for TextDocumentItem
+    ///
+    /// @return TextDocumentItem
+    ///
+    //////////////////////////////////////////////////////////////
+    TextDocumentItem getTextDocumentItem() const;
 
   private:
-    TextDocumentItem m_textDocumentItem;
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Mutator method for TextDocumentItem
+    ///
+    /// @param jsonRPC
+    ///
+    //////////////////////////////////////////////////////////////
+    void setTextDocumentItemKind(const nlohmann::json &jsonRPC);
+
+  private:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Contains the details of the text document that was opened,
+    ///        such as URI, language, version, and content.
+    ///
+    //////////////////////////////////////////////////////////////
+    std::shared_ptr<TextDocumentItem> m_textDocumentItem;
 };
+
 } // namespace justanlsp
