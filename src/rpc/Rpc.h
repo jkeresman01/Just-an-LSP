805087ad833ce92ab51b9e37a2dcd0dae8dc2980
josip <josipkeresman@gmail.com>
2024-10-27 12:16:58 +0100

    Remove debugging stuff from Rpc

--- src/rpc/Rpc.h
+++ src/rpc/Rpc.h
@@ -4,6 +4,7 @@
 
 #include "../enums/RequestType.h"
 #include "../messages/ResponseMessage.h"
+#include "../utils/Convert.h"
 #include "../utils/Logger.h"
 
 namespace justanlsp
@@ -15,48 +16,32 @@ class Rpc
 
     static std::string read()
     {
-        LOG_INFO << "Starting to read new message ..";
-
         std::string header;
         uint32_t contentLength = 0;
 
         while (getline(std::cin, header))
         {
-            LOG_INFO << "Received header line: '" << header << "'";
-
             header.erase(0, header.find_first_not_of(" \t\r\n\v\f"));
             header.erase(header.find_last_not_of(" \t\r\n\v\f") + 1);
 
             if (header.substr(0, 16) == "Content-Length: ")
             {
                 contentLength = std::stoul(header.substr(16));
-                LOG_INFO << "Extracted Content-Length: " << contentLength;
             }
 
             if (header.empty())
             {
-                LOG_INFO << "End of headers reached.";
                 break;
             }
         }
 
         if (contentLength == 0)
         {
-            LOG_ERROR << "No valid content length found, can't read the payload!";
             return "";
         }
 
-        LOG_INFO << "Reading message content with length: " << contentLength;
         std::string content(contentLength, ' ');
         std::cin.read(&content[0], contentLength);
-
-        if (!std::cin)
-        {
-            LOG_ERROR << "Error occurred while reading message content.";
-            return "";
-        }
-
-        LOG_INFO << "Successfully read message content: " << content;
 
         return content;
     }
