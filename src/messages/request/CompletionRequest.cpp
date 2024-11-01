df43b46d6c8f98f8c460a4193563febcd84d46d5
josip <josipkeresman@gmail.com>
2024-11-01 23:23:56 +0100

    Implement textDocument/completion req

---
+++ src/messages/request/CompletionRequest.cpp
@@ -0,0 +1,26 @@
+#include "CompletionRequest.h"
+
+#include "../../utils/Logger.h"
+
+namespace justanlsp
+{
+CompletionRequest::CompletionRequest(const nlohmann::json &jsonRPC)
+{
+    setParams(jsonRPC);
+}
+
+void CompletionRequest::setParams(const nlohmann::json &jsonRPC)
+{
+    auto it = jsonRPC.find("params");
+
+    if (it == jsonRPC.end())
+    {
+        LOG_ERROR("No params in request with method: textDocument/completion");
+    }
+    else
+    {
+        m_completionParams = std::make_shared<CompletionParams>(jsonRPC["params"]);
+    }
+}
+
+} // namespace justanlsp
