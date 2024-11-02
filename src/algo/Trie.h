468a36b5d8fa296834fe8c9e758f61987a5ea23a
josip <josipkeresman@gmail.com>
2024-11-02 19:00:17 +0100

    Make findAllWords() const in Trie impl

--- src/algo/Trie.h
+++ src/algo/Trie.h
@@ -21,7 +21,7 @@ class Trie
     std::shared_ptr<TrieNode> getTrieNode(const std::string &prefix) const;
 
     void findAllWords(std::shared_ptr<TrieNode> node, const std::string &prefix,
-                      std::vector<std::string> &results);
+                      std::vector<std::string> &results) const;
 
   private:
     std::shared_ptr<TrieNode> m_root = std::make_shared<TrieNode>();
