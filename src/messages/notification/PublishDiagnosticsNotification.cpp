ac17d78b9cde5503be938bf4ce9c26627fff4dc3
josip <josipkeresman@gmail.com>
2024-11-02 14:16:02 +0100

    Implement PublishDiagnosticsNotification msg

---
+++ src/messages/notification/PublishDiagnosticsNotification.cpp
@@ -0,0 +1,28 @@
+#include "PublishDiagnosticsNotification.h"
+
+namespace justanlsp
+{
+
+PublishDiagnosticsNoticifation::PublishDiagnosticsNoticifation(
+    std::string method, const std::shared_ptr<PublishDiagnosticsParams> &diagnosticsParams)
+    : NotificationMessage(method), m_diagnosticsParams(diagnosticsParams)
+{
+}
+
+nlohmann::json PublishDiagnosticsNoticifation::toJson() const
+{
+    return {{"jsonrpc", m_jsonRPC}, {"method", m_method}, {"params", m_diagnosticsParams->toJson()}};
+}
+
+std::ostream &operator<<(std::ostream &os,
+                         const PublishDiagnosticsNoticifation &publishDiagsnosticsNotification)
+{
+    nlohmann::json jsonRPC = publishDiagsnosticsNotification.toJson();
+
+    os << "Content-Length: " << jsonRPC.dump().size() << "\r\n\r\n";
+    os << jsonRPC.dump() << std::endl;
+
+    return os;
+}
+
+} // namespace justanlsp
---
+++ src/messages/notification/PublishDiagnosticsNotification.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <ostream>
+
+#include "../../params/PublishDiagnosticsParams.h"
+#include "NotificationMessage.h"
+
+namespace justanlsp
+{
+class PublishDiagnosticsNoticifation : public NotificationMessage
+{
+  public:
+    PublishDiagnosticsNoticifation(std::string method,
+                                   const std::shared_ptr<PublishDiagnosticsParams> &diagnosticsParams);
+    nlohmann::json toJson() const override;
+
+    friend std::ostream &operator<<(std::ostream &os,
+                                    const PublishDiagnosticsNoticifation &publishDiagsnosticsNotification);
+
+    std::shared_ptr<PublishDiagnosticsParams> getParams() const { return m_diagnosticsParams; };
+
+  private:
+    std::shared_ptr<PublishDiagnosticsParams> m_diagnosticsParams;
+};
+} // namespace justanlsp
