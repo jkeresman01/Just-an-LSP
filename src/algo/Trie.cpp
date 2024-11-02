ee4bb4786ccdcad044a41d8a3250a656ab42f74d
josip <josipkeresman@gmail.com>
2024-11-02 19:02:36 +0100

    Change int to size_t in Trie impl

--- src/algo/Trie.cpp
+++ src/algo/Trie.cpp
@@ -62,7 +62,7 @@ void Trie::findAllWords(std::shared_ptr<TrieNode> node, const std::string &prefi
         results.push_back(prefix);
     }
 
-    for (int i = 0; i < MAX_CHAR_NO; ++i)
+    for (size_t i = 0; i < MAX_CHAR_NO; ++i)
     {
         if (node->children[i] != nullptr)
         {
