3d9a427e39512ec3fac1997dd1d2b366a143a1b8
josip <josipkeresman@gmail.com>
2024-11-03 18:02:33 +0100

    Write tests for JustAnLSPClientTest

---
+++ test/core/JustAnLSPClientTest.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+
+#include "../../src/capabilities/ClientCapabilites.h"
+#include "../../src/core/JustAnLSPClient.h"
+#include "../../src/types/ClientInfo.h"
+
+#include <memory>
+
+namespace justanlsp
+{
+class JustAnLSPClientTest : public ::testing::Test
+{
+  protected:
+    ClientInfo mockClientInfo{"TestClient", "1"};
+    std::shared_ptr<ClientCapabilities> mockCapabilities = std::make_shared<ClientCapabilities>();
+    JustAnLSPClient client{mockClientInfo, mockCapabilities};
+};
+
+TEST_F(JustAnLSPClientTest, WillSaveClientInfoCorrectly)
+{
+    // GIVEN
+    ClientInfo newClientInfo{"NewClient", "2"};
+
+    // WHEN
+    client.saveInfo(newClientInfo);
+
+    // THEN
+    EXPECT_EQ(client.getInfo(), newClientInfo.toString());
+}
+
+TEST_F(JustAnLSPClientTest, WillAddDocumentCorrectly)
+{
+    // GIVEN
+    std::string URI = "document1";
+    std::string documentContent = "Ahaahaha tada tatarata";
+
+    // WHEN
+    client.addDocument(URI, documentContent);
+
+    // THEN
+    EXPECT_EQ(client.getDocumentByURI(URI), documentContent);
+}
+
+TEST_F(JustAnLSPClientTest, WillLogErrorForUnknownURI)
+{
+    // GIVEN
+    std::string unknownURI = "Kolinda";
+
+    // WHEN
+    std::string result = client.getDocumentByURI(unknownURI);
+
+    // THEN
+    EXPECT_EQ(result, "");
+}
+
+TEST_F(JustAnLSPClientTest, WillUpdateDocumentCorrectly)
+{
+    // GIVEN
+    std::string URI = "document1";
+    std::string initialContent = "Initial content";
+    client.addDocument(URI, initialContent);
+
+    // WHEN
+    std::string updatedContent = "Updated content";
+    client.updateDocumentByURI(URI, updatedContent);
+
+    // THEN
+    EXPECT_EQ(client.getDocumentByURI(URI), updatedContent);
+}
+
+} // namespace justanlsp
