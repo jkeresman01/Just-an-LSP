78a691127d95ff23dbe626617a45fab8fb62d38a
josip <josipkeresman@gmail.com>
2024-11-03 13:48:25 +0100

    Implement tests for CompletionResponse msg

---
+++ test/messages/CompletionResponseTest.cpp
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <sstream>
+
+#include "../../src/messages/response/CompletionResponse.h"
+#include "../../src/results/CompletionResult.h"
+
+using json = nlohmann::json;
+
+namespace justanlsp
+{
+class CompletionResponseTest : public ::testing::Test
+{
+  protected:
+    int64_t mockId = 1;
+    std::string mockJsonRPC = "2.0";
+
+    std::vector<CompletionItem> completionItems{
+        {"dnsClient", "DNS client test 1", "DNS client test 1 documentation"},
+        {"dnsClientId", "DNS client test 1", "DNS client id test 1 documentation"},
+        {"dnsClientIpAddress", "DNS client ip address test 1", "DNS client ip address test 1 documentation"},
+    };
+
+    CompletionResult mockCompletionResult{completionItems};
+};
+
+TEST_F(CompletionResponseTest, WillConvertToJsonCorrectly)
+{
+    // GIVEN
+    CompletionResponse response(mockJsonRPC, mockId, mockCompletionResult);
+
+    // WHEN
+    json resultJson = response.toJson();
+
+    // THEN
+    EXPECT_EQ(resultJson["jsonrpc"], mockJsonRPC);
+    EXPECT_EQ(resultJson["id"], mockId);
+    EXPECT_EQ(resultJson["result"], mockCompletionResult.toJson());
+}
+
+TEST_F(CompletionResponseTest, WillOutputStreamCorrectly)
+{
+    // GIVEN
+    CompletionResponse response(mockJsonRPC, mockId, mockCompletionResult);
+
+    // WHEN
+    std::ostringstream oss;
+    oss << response;
+
+    std::string output = oss.str();
+
+    // THEN
+    json expectedJson = response.toJson();
+    std::string expectedOutput = "Content-Length: " + std::to_string(expectedJson.dump().size()) +
+                                 "\r\n\r\n" + expectedJson.dump() + "\n";
+
+    EXPECT_EQ(output, expectedOutput);
+}
+} // namespace justanlsp
