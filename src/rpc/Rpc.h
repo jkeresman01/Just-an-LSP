c55ab33ea454e8cc88f0d42f3d4c0fd0c26c22b6
josip <josipkeresman@gmail.com>
2024-12-18 22:19:24 +0100

    Refactor Rpc module impl

--- src/rpc/Rpc.h
+++ src/rpc/Rpc.h
@@ -9,44 +9,83 @@
 
 namespace justanlsp
 {
+/////////////////////////////////////////////////////////////////////
+///
+/// @class Rpc
+///
+/// @brief Responsible for read and sending messages defined by
+///        language server protocol
+///
+/////////////////////////////////////////////////////////////////////
 class Rpc
 {
   public:
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Delete default constructor to prevent instantiation
+    ///
+    /////////////////////////////////////////////////////////////////////
     Rpc() = delete;
 
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Send Message defined by language server protocol
+    ///
+    /// @param response
+    ///
+    /////////////////////////////////////////////////////////////////////
     static void send(const Message &response) { std::cout << response << std::endl; }
 
-    static std::string read()
-    {
-        uint32_t contentLength = readContentLength();
-
-        std::string content(contentLength, ' ');
-        std::cin.read(&content[0], contentLength);
-
-        return content;
-    }
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Read JSON RPC request
+    ///
+    /////////////////////////////////////////////////////////////////////
+    static std::string read();
 
   private:
-    static uint32_t readContentLength()
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Read content length from JSON RPC request
+    ///
+    /////////////////////////////////////////////////////////////////////
+    static uint32_t readContentLength();
+};
+
+/////////////////////////////////////////////////////////////////////
+/// Implementation of inline defined functions
+/////////////////////////////////////////////////////////////////////
+
+inline std::string Rpc::read()
+{
+    uint32_t contentlength = readContentLength();
+
+    std::string content(contentlength, ' ');
+    std::cin.read(&content[0], contentlength);
+
+    return content;
+}
+
+inline uint32_t Rpc::readContentLength()
+{
+    std::string header;
+    uint32_t contentLength = 0;
+
+    while (getline(std::cin, header))
     {
-        std::string header;
-        uint32_t contentLength = 0;
+        header.erase(0, header.find_first_not_of(" \t\r\n\v\f"));
+        header.erase(header.find_last_not_of(" \t\r\n\v\f") + 1);
 
-        while (getline(std::cin, header))
+        if (header.substr(0, 16) == "Content-Length: ")
         {
-            header.erase(0, header.find_first_not_of(" \t\r\n\v\f"));
-            header.erase(header.find_last_not_of(" \t\r\n\v\f") + 1);
+            contentLength = std::stoul(header.substr(16));
+        }
 
-            if (header.substr(0, 16) == "Content-Length: ")
-            {
-                contentLength = std::stoul(header.substr(16));
-            }
-
-            if (header.empty())
-            {
-                return contentLength;
-            }
+        if (header.empty())
+        {
+            return contentLength;
         }
     }
-};
+}
+
 } // namespace justanlsp
