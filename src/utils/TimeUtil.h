bd7a7660f23e048fdf1329e4a117c0aa3d5b088e
josip <josipkeresman@gmail.com>
2024-12-19 23:13:15 +0100

    Document time utility

--- src/utils/TimeUtil.h
+++ src/utils/TimeUtil.h
@@ -8,21 +8,56 @@
 namespace justanlsp
 {
 
+/////////////////////////////////////////////////////////////////////
+///
+/// @class TimeUtil
+///
+/// @brief Utility class for time related stuff
+///
+/////////////////////////////////////////////////////////////////////
 class TimeUtil
 {
   public:
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Delete default constructor to prevent instantiation
+    ///
+    /////////////////////////////////////////////////////////////////////
     TimeUtil() = delete;
 
-    static std::time_t now()
-    {
-        std::chrono::time_point<std::chrono::high_resolution_clock> currentTime =
-            std::chrono::high_resolution_clock::now();
-        return std::chrono::high_resolution_clock::to_time_t(currentTime);
-    }
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Gets the current time
+    ///
+    /////////////////////////////////////////////////////////////////////
+    static std::time_t now();
 
-    static std::ostream &putTime(std::ostream &out, const std::time_t &time)
-    {
-        return out << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
-    }
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Print and format provided time
+    ///
+    /// @param out
+    /// @param time
+    ///
+    /////////////////////////////////////////////////////////////////////
+    static std::ostream &putTime(std::ostream &out, const std::time_t &time);
 };
+
+/////////////////////////////////////////////////////////////////////
+/// Implementation of inline defined functions
+/////////////////////////////////////////////////////////////////////
+
+inline std::time_t TimeUtil::now()
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> currentTime =
+        std::chrono::high_resolution_clock::now();
+    return std::chrono::high_resolution_clock::to_time_t(currentTime);
+}
+
+static std::ostream &putTime(std::ostream &out, const std::time_t &time)
+{
+    return out << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+}
+
+
 } // namespace justanlsp
