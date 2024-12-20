e982baacd38330f862a38b618af6f97ac95974b1
josip <josipkeresman@gmail.com>
2024-12-20 22:12:39 +0100

    Document trace utility class

--- src/utils/TraceUtil.h
+++ src/utils/TraceUtil.h
@@ -6,24 +6,52 @@
 
 namespace justanlsp
 {
+
+/////////////////////////////////////////////////////////////////////
+///
+/// @class TraceUtil
+///
+/// @brief Utility class for trace related stuff
+///
+/////////////////////////////////////////////////////////////////////
 class TraceUtil
 {
   public:
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Delete default constructor to prevent instantiation
+    ///
+    /////////////////////////////////////////////////////////////////////
     TraceUtil() = delete;
 
-    static TraceValue getTraceValue(const std::string &traceValueStr)
-    {
-        if (traceValueStr == "messages")
-        {
-            return TraceValue::MESSAGES;
-        }
-
-        if (traceValueStr == "verbose")
-        {
-            return TraceValue::VERBOSE;
-        }
-
-        return TraceValue::OFF;
-    }
+    /////////////////////////////////////////////////////////////////////
+    ///
+    /// @brief Converts string trace value to enum representation
+    ///
+    /// @param traceValueStr
+    ///
+    /////////////////////////////////////////////////////////////////////
+    static TraceValue getTraceValue(const std::string &traceValueStr);
 };
+
+/////////////////////////////////////////////////////////////////////
+/// Implementation of inline defined functions
+/////////////////////////////////////////////////////////////////////
+
+TraceValue inline getTraceValue(const std::string &traceValueStr)
+{
+    if (traceValueStr == "messages")
+    {
+        return TraceValue::MESSAGES;
+    }
+
+    if (traceValueStr == "verbose")
+    {
+        return TraceValue::VERBOSE;
+    }
+
+    return TraceValue::OFF;
+}
+
+
 } // namespace justanlsp
