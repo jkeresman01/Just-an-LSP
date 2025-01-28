b429feb0e62d4b55a4de111ceac43b3a3e722bbc
josip <josipkeresman@gmail.com>
2025-01-28 23:26:51 +0100

    Document IDiagnosticsProvider

--- src/Diagnostics/IDiagnosticsProvider.h
+++ src/Diagnostics/IDiagnosticsProvider.h
@@ -7,9 +7,31 @@
 
 namespace justanlsp
 {
+//////////////////////////////////////////////////////////////
+///
+/// @class IDiagnosticsProvider
+///
+/// @brief An interface for providing diagnostics for a 
+///        given document and position.
+///
+//////////////////////////////////////////////////////////////
 class IDiagnosticsProvider
 {
   public:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Retrieves a list of diagnostics for a given 
+    ///        document and position.
+    ///
+    /// @param document 
+    /// @param position
+    ///
+    /// @return A vector of Diagnostic objects representing the 
+    ///         diagnostics for the specified document and position.
+    ///
+    //////////////////////////////////////////////////////////////
     virtual std::vector<Diagnostic> getDiagnostics(const std::string &document, const Position &position) = 0;
 };
+
 } // namespace justanlsp
+
