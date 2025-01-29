b2c6b35376da4dfd9b8c14803e1ec058630047a7
josip <josipkeresman@gmail.com>
2025-01-29 23:23:13 +0100

    Add tests for XML utility class

---
+++ test/Utils/XMLUtilTest.cpp
@@ -0,0 +1,58 @@
+#include "../../src/Utils/XMLUtil.h"
+#include <gtest/gtest.h>
+
+using namespace justanlsp;
+
+class XMLUtilTest : public ::testing::Test
+{
+};
+
+TEST_F(XMLUtilTest, WillParseValidPropertyLine)
+{
+    // GIVEN
+    std::string line = "<property name=\"username\">MilicaKrmpotic</property>";
+
+    // WHEN
+    auto result = XMLUtil::parseFromFileLine(line);
+
+    // THEN
+    EXPECT_EQ(result.first, "username");
+    EXPECT_EQ(result.second, "MilicaKrmpotic");
+}
+
+TEST_F(XMLUtilTest, WillReturnEmptyPairForInvalidPropertyLine)
+{
+    // GIVEN
+    std::string line = "<property name=>Milica Krmpotic</property>";
+
+    // WHEN
+    auto result = XMLUtil::parseFromFileLine(line);
+
+    // THEN
+    EXPECT_TRUE(result.first.empty());
+    EXPECT_TRUE(result.second.empty());
+}
+
+TEST_F(XMLUtilTest, WillValidateCorrectXMLLine)
+{
+    // GIVEN
+    std::string_view line = "<property name=\"username\">Milica Krmpotic</property>";
+
+    // WHEN
+    bool result = XMLUtil::isValidXMLLine(line);
+
+    // THEN
+    EXPECT_TRUE(result);
+}
+
+TEST_F(XMLUtilTest, WillInvalidateIncorrectXMLLine)
+{
+    // GIVEN
+    std::string_view line = "<property name=\"username\">Milica Krmpotic";
+
+    // WHEN
+    bool result = XMLUtil::isValidXMLLine(line);
+
+    // THEN
+    EXPECT_FALSE(result);
+}
