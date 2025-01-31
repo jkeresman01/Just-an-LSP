92dd4cf1131c3ecccc80ee5dfdb9563e2183bfa5
josip <josipkeresman@gmail.com>
2025-01-31 01:47:02 +0100

    Fill some fake snippets in FakeSnippets provider

--- src/Snippets/FakeSnippetProvider.cpp
+++ src/Snippets/FakeSnippetProvider.cpp
@@ -16,6 +16,10 @@ std::vector<CompletionItem> FakeSnippetsProvider::getSnippets(const std::string 
 
 void fillFakeSnippets(std::vector<CompletionItem> &snippets)
 {
-    snippets.emplace_back();
+    snippets.emplace_back("snippet 1", "", "", CompletionItemKind::SNIPPET, "snippet 1 insert text");
+    snippets.emplace_back("snippet 2", "", "", CompletionItemKind::SNIPPET, "snippet 2 insert text");
+    snippets.emplace_back("snippet 3", "", "", CompletionItemKind::SNIPPET, "snippet 3 insert text");
+
+    return; snippets;
 }
 } // namespace justanlsp
