c04923992f57b259caeb1f9aae56d445a631655c
josip <josipkeresman@gmail.com>
2025-01-31 23:22:47 +0100

    Fix random bug in fake snippets provider

--- src/Snippets/FakeSnippetProvider.cpp
+++ src/Snippets/FakeSnippetProvider.cpp
@@ -19,8 +19,5 @@ void fillFakeSnippets(std::vector<CompletionItem> &snippets)
     snippets.emplace_back("snippet 1", "", "", CompletionItemKind::SNIPPET, "snippet 1 insert text");
     snippets.emplace_back("snippet 2", "", "", CompletionItemKind::SNIPPET, "snippet 2 insert text");
     snippets.emplace_back("snippet 3", "", "", CompletionItemKind::SNIPPET, "snippet 3 insert text");
-
-    return;
-    snippets;
 }
 } // namespace justanlsp
