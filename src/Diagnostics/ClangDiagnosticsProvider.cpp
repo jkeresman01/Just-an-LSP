a3e39d0974dcba6b6f01ff3913bc46f0a3e30ca3
josip <josipkeresman@gmail.com>
2025-02-03 19:35:40 +0100

    Implement some stuff for clang diagnostics

--- src/Diagnostics/ClangDiagnosticsProvider.cpp
+++ src/Diagnostics/ClangDiagnosticsProvider.cpp
@@ -1,11 +1,60 @@
 #include "ClangDiagnosticsProvider.h"
 
+#include <clang/AST/AST.h>
+#include <clang/Basic/Diagnostic.h>
+#include <clang/Basic/FileManager.h>
+#include <clang/Basic/SourceManager.h>
+#include <clang/Frontend/ASTConsumers.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/ParseableAST.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
+#include <clang/Parse/Parser.h>
+#include <clang/Tooling/Tooling.h>
+
 namespace justanlsp
 {
-std::vector<Diagnostic> ClangDiagnosticsProvider::getDiagnostics(const std::string &document,
-                                                                 const Position &position)
+
+ClangDiagnosticsProvider::ClangDiagnosticsProvider()
 {
-    // TODO implement this stuff
+    m_diagnosticsPrinter = std::make_unique<clang::TextDiagnosticPrinter>(llvm::errs(), &DiagOpts);
+    m_diagnosticsEngine = m_compilerInstance.getDiagnostics();
+    m_diagnosticsEngine.setClient(m_diagnosticsPrinter.get());
+    m_fileManager = m_compilerInstance.getFileManager();
+    m_sourceManager = m_compilerInstance.getSourceManager();
+    m_memmoryBuffer = llvm::MemoryBuffer::getMemBuffer(document);
+    m_fileID =
+        m_sourceManager.createFileID(std::move(buffer), clang::SourceLocation(), clang::SrcMgr::C_User);
+    m_compilerInstance.createPreprocessor(clang::TU_Complete);
+    m_astConsumer = std::make_unique<clang::ASTConsumer>();
+}
+
+std::vector<Diagnostic> ClangDiagnosticsProvider::getDiagnostics(const std::string &document)
+{
+    clang::ParseAST(m_compilerInstance.getPreprocessor(), m_astConsumer.get(),
+                    m_compilerInstance.getASTContext());
+
+    std::vector<Diagnostic> diagnostics;
+    unsigned numDiagnostics = m_diagnosticsEngine.getNumDiagnostics();
+
+    for (unsigned i = 0; i < numDiagnostics; ++i)
+    {
+        clang::Diagnostic &clangDiagnostics = m_diagnosticsEngine.getDiagnostic(i);
+        Diagnostic justAnLSPDiagnostic;
+
+        justAnLSPDiagnostic.message =
+            clang::DiagnosticIDs::getDiagnosticString(justAnLSPDiagnosticnostic.getID());
+        justAnLSPDiagnostic.severity =
+            static_cast<DiagnosticSeverity>(justAnLSPDiagnosticnostic.getSeverity());
+
+        clang::SourceLocation location = justAnLSPDiagnosticnostic.getLocation();
+        clang::SourceRange range = justAnLSPDiagnosticnostic.getRange();
+
+        justAnLSPDiagnostic.range = Range(location, range.getEnd());
+
+        justAnLSPDiagnosticnostics.push_back(justAnLSPDiagnostic);
+    }
+
+    return diagnostics;
 }
 
 } // namespace justanlsp
--- src/Diagnostics/ClangDiagnosticsProvider.h
+++ src/Diagnostics/ClangDiagnosticsProvider.h
@@ -4,6 +4,17 @@
 
 #include "IDiagnosticsProvider.h"
 
+#include <clang/AST/AST.h>
+#include <clang/Basic/Diagnostic.h>
+#include <clang/Basic/FileManager.h>
+#include <clang/Basic/SourceManager.h>
+#include <clang/Frontend/ASTConsumers.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/ParseableAST.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
+#include <clang/Parse/Parser.h>
+#include <clang/Tooling/Tooling.h>
+
 namespace justanlsp
 {
 
@@ -18,6 +29,8 @@ namespace justanlsp
 class ClangDiagnosticsProvider : public IDiagnosticsProvider
 {
   public:
+    ClangDiagnosticsProvider();
+
     //////////////////////////////////////////////////////////////
     ///
     /// @brief Retrieves diagnostics for a given document at a
@@ -31,6 +44,19 @@ class ClangDiagnosticsProvider : public IDiagnosticsProvider
     ///
     //////////////////////////////////////////////////////////////
     std::vector<Diagnostic> getDiagnostics(const std::string &document, const Position &position) override;
+
+  private:
+    clang::CompilerInstance m_compilerInstance;
+    clang::DiagnosticOptions m_diagnosticsTag;
+    clang::DiagnosticsEngine m_diagnosticsEngine;
+    clang::FileManager m_fileManager;
+    clang::SourceManager m_sourceManager;
+    clang::FileID m_fileID;
+
+    llvm::MemoryBuffer *m_memmoryBuffer;
+
+    std::unique_ptr<clang::TextDiagnosticPrinter> m_diagnosticsPrinter;
+    std::unique_ptr<clang::ASTConsumer> m_astConsumer;
 };
 
 } // namespace justanlsp
